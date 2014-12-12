53b30e66a89c16306990a697847e70c7c93ee73c
John Vorster <bluglasshead@gmail.com>
2014-12-12 22:58:55 +0000

    some modifications

--- rock_band.py
+++ rock_band.py
@@ -1,7 +1,6 @@
 class Musician(object):
-    def __init__(self, sounds, name):
+    def __init__(self, sounds):
         self.sounds = sounds
-        self.name = name
 
     def solo(self, length):
         for i in range(length):
@@ -27,12 +26,15 @@ class Drummer(Musician):
         print "Be with you in a moment"
         print "Twoning, sproing, splang"
 
-#class Band(Musician):
-#    pass
+class Band(Musician):
+    #band_members = add(Musician)
+    pass
+    
 
 def main():
     print "I am looking to start a band! Tell me about yourself and play a little. If I like you, you're in!"
     band_no = 1 # thats me, the lead singer
+    the_band = []
 
     while band_no < 4:
       applicant = raw_input("What is your name? ")
@@ -48,11 +50,13 @@ def main():
       print "Let's here you play %s.", applicant #not quite getting the issue / solution here
       applicant.solo(6)
 
-      in_or_out = raw_input("Hmmm, let me think, so I add you or not? ")
+      in_or_out = raw_input("Hmmm, let me think, so I add you or not? (y or n): ")
       
       if in_or_out == "y":
         band_no += 1
         print "You know, I kinnda like you and you can play a decent tune there. You're in!"
+        the_band.append(applicant)
+        print the_band
       else:
         print "I'm sorry, I don't think you are what the band needs right now. Keep on jamming, you'll hit the big time one day."
       
